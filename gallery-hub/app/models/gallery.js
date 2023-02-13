00083521394e76dcd02f4c759253a6a5a36d9826
Briana Wright <brianaleew@gmail.com>
2023-02-13 12:20:50 -0500

    Added gallery schema and routes (not finished) and edited server.js to expect future route/model additions

---
+++ gallery-hub/app/models/gallery.js
@@ -0,0 +1,34 @@
+//This is the schema for Galleries 
+// REMINDER !!
+//Featured Artists will be a virtual that we add after we have artists to populate the area!!
+
+const mongoose = require('mongoose')
+
+const gallerySchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+        description: {
+            type: String,
+            required: true, 
+        },
+		location: String,
+        img: String,
+        curators: Array,
+        artworks: [artworkSchema],
+		owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+
+
+module.exports = mongoose.model('Gallery', gallerySchema)
--- gallery-hub/server.js
+++ gallery-hub/server.js
@@ -4,8 +4,12 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
-const exampleRoutes = require('./app/routes/example_routes')
+const galleryRoutes = require('./app/routes/gallery_routes')
 const userRoutes = require('./app/routes/user_routes')
+const artistRoutes = require('./app/routes/artist_routes')
+const artworkRoutes = require('./app/routes/artwork_routes')
+
+
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -65,8 +69,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
-app.use(exampleRoutes)
+app.use(galleryRoutes)
 app.use(userRoutes)
+app.use(artistRoutes)
+app.use(artworkRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be

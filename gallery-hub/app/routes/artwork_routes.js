97836b942df22135ab9cfeaf313207b9484c99d0
Eric Elsner <ericelsnercodes@gmail.com>
2023-02-14 10:01:16 -0500

    back end seed_artwork file. run with node from terminal to pull 5 random objectIDs for each artist or culture in the queryParams array.

--- gallery-hub/app/routes/artwork_routes.js
+++ gallery-hub/app/routes/artwork_routes.js
@@ -18,6 +18,7 @@ const router = express.Router()
 
 // ROUTES
 
+// CREATE/ADD Artwork to gallery
 router.post('/artworks/:galleryId', removeBlanks, (req, res, next) => {
     // isolate our artwork from the request, and save to variable
     const artwork = req.body.artwork
@@ -45,54 +46,63 @@ router.post('/artworks/:galleryId', removeBlanks, (req, res, next) => {
 
 // PATCH -> update a artwork
 // PATCH /artworks/:galleryId/:artworkId
-router.patch('/artworks/:galleryId/:artworkId', requireToken, removeBlanks, (req, res, next) => {
-    // get and save the id's to variables
-    const galleryId = req.params.galleryId
-    const artworkId = req.params.artworkId
+router.patch(
+    '/artworks/:galleryId/:artworkId',
+    requireToken,
+    removeBlanks,
+    (req, res, next) => {
+        // get and save the id's to variables
+        const galleryId = req.params.galleryId
+        const artworkId = req.params.artworkId
 
-    // find our gallery
-    Gallery.findById(galleryId)
-        .then(handle404)
-        .then(gallery => {
-            // single out the artwork
-            const theArtwork = gallery.artworks.id(artworkId)
-            // make sure the user is the gallery's owner
-            requireOwnership(req, gallery)
-            // update accordingly
-            theArtwork.set(req.body.artwork)
+        // find our gallery
+        Gallery.findById(galleryId)
+            .then(handle404)
+            .then(gallery => {
+                // single out the artwork
+                const theArtwork = gallery.artworks.id(artworkId)
+                // make sure the user is the gallery's owner
+                requireOwnership(req, gallery)
+                // update accordingly
+                theArtwork.set(req.body.artwork)
 
-            return gallery.save()
-        })
-        // send a status
-        .then(() => res.sendStatus(204))
-        .catch(next)
-})
+                return gallery.save()
+            })
+            // send a status
+            .then(() => res.sendStatus(204))
+            .catch(next)
+    }
+)
 
 // DELETE -> destroy a artwork
 // DELETE /artworks/:galleryId/:artworkId
-router.delete('/artworks/:galleryId/:artworkId', requireToken, (req, res, next) => {
-    const galleryId = req.params.galleryId
-    const artworkId = req.params.artworkId
+router.delete(
+    '/artworks/:galleryId/:artworkId',
+    requireToken,
+    (req, res, next) => {
+        const galleryId = req.params.galleryId
+        const artworkId = req.params.artworkId
 
-    // find the gallery
-    Gallery.findById(galleryId)
-        .then(handle404)
-        // grab the specific artwork using it's id
-        .then(gallery => {
-            // isolate the artwork
-            const theArtwork = gallery.artworks.id(artworkId)
-            // make sure the user is the owner of the gallery
-            requireOwnership(req, gallery)
-            // call remove on our artwork subdoc
-            theArtwork.remove()
-            // return the saved gallery
-            return gallery.save()
-        })
-        // send a response
-        .then(() => res.sendStatus(204))
-        // pass errors to our error handler (using next)
-        .catch(next)
-})
+        // find the gallery
+        Gallery.findById(galleryId)
+            .then(handle404)
+            // grab the specific artwork using it's id
+            .then(gallery => {
+                // isolate the artwork
+                const theArtwork = gallery.artworks.id(artworkId)
+                // make sure the user is the owner of the gallery
+                requireOwnership(req, gallery)
+                // call remove on our artwork subdoc
+                theArtwork.remove()
+                // return the saved gallery
+                return gallery.save()
+            })
+            // send a response
+            .then(() => res.sendStatus(204))
+            // pass errors to our error handler (using next)
+            .catch(next)
+    }
+)
 
 // export our router
 module.exports = router

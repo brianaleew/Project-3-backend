807e824fe9c9b949c814679c6dcd1c598c76742b
Briana Wright <brianaleew@gmail.com>
2023-02-20 20:29:13 -0500

    Edited seeds for artists

--- app/models/seed_artists.js
+++ app/models/seed_artists.js
@@ -4,25 +4,26 @@ const db = require('../../config/db')
 
 const starterArtists = [
     {
-        name: 'Briana Wright',
-        description: 'An artist',
-        location: 'City,State',
-        website: 'www.google.com',
-        img: '/power.jpeg', 
+        name: 'Caravaggio',
+        description: 'One of the  leading Italian painter of the late 16th and early 17th centuries who became famous for the intense and unsettling realism of his large-scale religious works.',
+        location: 'Milan, Italy',
+        website: 'https://www.caravaggio.org/',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Bild-Ottavio_Leoni%2C_Caravaggio.jpg/220px-Bild-Ottavio_Leoni%2C_Caravaggio.jpg', 
+        
     },
     {
-        name: 'Eric Elsner',
-        description: 'An artist',
-        location: 'City,State',
-        website: 'www.google.com',
-        img: '/power.jpeg', 
+        name: 'Frida Kahlo',
+        description: 'Frida Kahlo was a a Mexican painter known for her many portraits, self-portraits, and works inspired by the nature and artifacts of Mexico.',
+        location: 'Mexico City, Mexico',
+        website: 'https://www.fridakahlo.org/',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTswCZvfUKzrNrXzOT359RTNi5lNBdwPrw1kw&usqp=CAU', 
     },
     {
-        name: 'Belal Elkurd',
-        description: 'An artist',
-        location: 'City,State',
-        website: 'www.google.com',
-        img: '/power.jpeg', 
+        name: 'Jean-Michel Basquiat',
+        description: 'Jean-Michel Basquiat was a Neo-Expressionist painter.His work explored his mixed African, Latinx, and American heritage through a visual vocabulary of personally resonant signs, symbols, and figures, and his art developed rapidly in scale, scope, and ambition as he moved from the street to the gallery.',
+        location: 'New York City, USA',
+        website: 'https://www.biography.com/artists/jean-michel-basquiat',
+        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Jean-Michel_Basquiat_1982_by_Andy_Warhol.png/220px-Jean-Michel_Basquiat_1982_by_Andy_Warhol.png', 
     },
 ]
 
